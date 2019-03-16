801c67ae8436768f16473c14c00d74980ef37bea
adeodato01 <arvin_deodato@hotmail.com>
2019-03-16 10:41:04 -0700

    initial Commit

---
+++ cSharpGold/Location.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cSharpGold
+{
+    /// <summary>
+    /// Place where various amounts of Gold are given / taken away
+    /// </summary>
+    class Location
+    {
+        #region Properties
+        /// <summary>
+        /// Name of the Location
+        /// </summary>
+        public string LocationName { get; set; }
+        /// <summary>
+        /// Amount of Gold to be given / taken away
+        /// </summary>
+        public decimal Gold { get; set; }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Calculations for the amount of Gold to be given / taken away
+        /// </summary>
+        public void GoldAmount()
+        {
+            /// calculations to determine how much gold given / taken away
+        }
+        
+        #endregion
+    }
+}
---
+++ cSharpGold/Scoreboard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cSharpGold
+{
+    /// <summary>
+    /// a Scoreboard to display accumulated Gold
+    /// </summary>
+    class Scoreboard
+    {
+        #region Properties
+        /// <summary>
+        /// Total tally of Gold
+        /// </summary>
+        public decimal TotalScore { get; set; }
+        #endregion
+
+        #region Methods
+        /// add methods to calculate the total Gold
+        #endregion
+    }
+}
