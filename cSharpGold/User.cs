b517950b2293e02f1c9e4ae19faafe0d2ad695b5
adeodato01 <arvin_deodato@hotmail.com>
2019-03-25 17:19:07 -0700

    added User class for recording / displaying a High Score

---
+++ cSharpGold/User.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cSharpGold
+{
+    /// <summary>
+    /// User class stores a High Score
+    /// </summary>
+    class User
+    {
+        #region Statics
+        private static int lastUserNumber = 0;
+        #endregion
+
+        #region Constructor
+        public User()
+        {
+            
+            UserNumber = ++lastUserNumber; 
+            CreatedDate = DateTime.Now;
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// User's high score
+        /// </summary>
+        public int HighScore { get; private set; }
+        /// <summary>
+        /// User number, private to the game
+        /// </summary>
+        public int UserNumber { get; private set; }
+        /// <summary>
+        /// User name set by user after exiting the game if score is among the 5 highest
+        /// </summary>
+        public string UserName { get; set; }
+        /// <summary>
+        /// User Created Date
+        /// </summary>
+        public DateTime CreatedDate { get; private set; }
+        #endregion
+    }
+}
