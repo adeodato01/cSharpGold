bfabac78e1fcbc0865427ca2e8ee817d03faf5ec
adeodato01 <arvin_deodato@hotmail.com>
2019-05-18 10:41:10 -0700

    removed commented out code

--- cSharpGold/Program.cs
+++ cSharpGold/Program.cs
@@ -6,13 +6,7 @@ namespace cSharpGold
     {
         static void Main(string[] args)
         {
-            /*
-            var L1 = Game.CreateLocation("Cave", 3, 15);
-
-            Console.WriteLine($"The location name is {L1.LocationName} " +
-                $"and the amount of Gold you get is {L1.Gold} !!!!!!!!!");
-                */
-
+        
             Console.WriteLine("********************");
             Console.WriteLine("Welcome to c# Gold!");
             Console.WriteLine("********************");
@@ -89,65 +83,6 @@ namespace cSharpGold
                         }
                         break;
                         
-
-
-
-
-                        /*
-                        try
-                        {
-                            
-                            var locationTypes = Enum.GetNames(typeof(LocationType));
-                            for (int i = 0; i < locationTypes.Length; i++)
-                            {
-                                Console.WriteLine($"{i + 1}. {locationTypes[i]}");
-                            }
-
-                            Console.Write("Choose a location: ");
-                            var locationType = Enum.Parse<LocationType>(Console.ReadLine());
-                            
-                            var l1 = Game.CreateLocation(locationType);
-
-                            Console.WriteLine($"Location Name: {l1.LocationName}, Gold Awarded: {l1.Gold}, Total Gold: l1.goldTotal"); //check out formatting like :C
-                        }
-                        catch (ArgumentException ax)
-                        {
-                            Console.WriteLine($"Location Type Error - {ax.Message} - Please try again!");
-                        }                      
-                        catch (Exception ex)
-                        {
-                            Console.WriteLine($"Sorry something went wrong - {ex.Message} - Please try again");
-                        }
-                        break;
-                        */
-
-
-                        /*
-                    case "2":
-                        PrintAllAccounts();
-                        Console.Write("AccountNumber: ");
-                        var accountNumber = Convert.ToInt32(Console.ReadLine());
-                        Console.Write("Amount to deposit: ");
-                        var depositAmount = Convert.ToDecimal(Console.ReadLine());
-                        Bank.Deposit(accountNumber, depositAmount);
-                        Console.WriteLine("Deposit successfully completed!");
-                        break;
-                    case "3":
-                        PrintAllAccounts();
-                        Console.Write("AccountNumber: ");
-                        accountNumber = Convert.ToInt32(Console.ReadLine());
-                        Console.Write("Amount to withdraw: ");
-                        var withdrawAmount = Convert.ToDecimal(Console.ReadLine());
-                        Bank.Withdraw(accountNumber, withdrawAmount);
-                        Console.WriteLine("Withdrawl successfully completed!");
-                        break;
-                    case "4":
-                        PrintAllAccounts();
-                        break;
-                        */
-
-
-
                     default:
                         break;
                 }
