6df7eb8f5492172060acc2a0fd3260f7218b451d
adeodato01 <arvin_deodato@hotmail.com>
2019-05-19 21:52:03 -0700

    UI project

---
+++ cSharpGoldUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cSharpGoldUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
